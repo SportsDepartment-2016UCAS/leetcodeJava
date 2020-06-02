58678ed0b890ccd547f29add95989beb7729a277
sdzbzj0406 <sdzbzj0406@sina.com>
2020-06-02 22:14:38 +0800

    六月第二天打卡

--- src/main/java/june/sumNums_64.java
+++ src/main/java/june/sumNums_64.java
@@ -4,8 +4,9 @@ public class sumNums_64 {
 
     public int sumNums(int n) {
 
-
+        // A&&B   A==True，则B肯定执行，所以A为递归条件，B为执行内容
         boolean flag = n > 0 && (n += sumNums(n - 1)) > 0;
+        //         boolean flag = n < 1 || (n += sumNums(n - 1)) < 2;
         return n;
 
     }
